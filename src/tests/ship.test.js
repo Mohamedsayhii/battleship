e288326d99ef91a4cb42d17d3aa1026f0cc72682
Mohamedsayhii <mohamedsaihii@outlook.fr>
2024-08-14 22:53:54 +0100

    Fix ship.test.js

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,28 +1,21 @@
-import ship from '../ship';
+import ship, { carrier, patrolBoat } from '../ship';
 
 describe('Ship unit tests', () => {
     test('should return hits', () => {
-        const carrier = ship(5);
         expect(carrier.getHits()).toBe(0);
     });
     test('should increment hits', () => {
-        const carrier = ship(5);
         carrier.hit();
         carrier.hit();
         expect(carrier.getHits()).toBe(2);
     });
     test('should check if a ship has not sunk', () => {
-        const carrier = ship(5);
         carrier.hit();
         expect(carrier.isSunk()).toBeFalsy();
     });
     test('should check if a ship has sunk', () => {
-        const carrier = ship(5);
-        carrier.hit();
-        carrier.hit();
-        carrier.hit();
-        carrier.hit();
-        carrier.hit();
-        expect(carrier.isSunk()).toBeTruthy();
+        patrolBoat.hit();
+        patrolBoat.hit();
+        expect(patrolBoat.isSunk()).toBeTruthy();
     });
 });
